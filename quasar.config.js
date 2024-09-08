ef3168154ddbb4041544ffb76c43da964b8d1b17
mrpwes <31194909+mrpwes@users.noreply.github.com>
2024-09-09 00:00:14 +0800

    refactor: Update publicPath in quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: "/deploy-a1-agro",
+      publicPath: "/deploy-a1-agro",
       // analyze: true,
       // env: {},
       // rawDefine: {}
